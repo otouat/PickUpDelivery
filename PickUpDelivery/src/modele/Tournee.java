e1245cbb3f880e3cb69d19e0929c0af1c05ea10f
otouat <ousmane.touat@insa-lyon.fr>
2019-10-21 23:13:10 +0200

    Update Tournee.java

--- PickUpDelivery/src/modele/Tournee.java
+++ PickUpDelivery/src/modele/Tournee.java
@@ -1,33 +1,95 @@
 package modele;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
 
 public class Tournee {
 	private Entrepot entrepot;
 	private Map<Integer, Livraison> livraisons;
-	private int[][] cout;
-	private int nombreNoeuds;
-	private Map<Integer, Noeud> correspNoeudEntier;
+	private int nombreNoeudAVisiter;
+	private HashMap<Integer, Noeud> mapNoeudsAVisiter;
+	private HashMap<Noeud,Integer> mapNoeuds;
+	private int[][] coutPlan;
+	private int[][] coutNoeudAVisiter;
 
 	public Tournee(Entrepot entrepot, Map<Integer, Livraison> livraisons) {
 		this.entrepot = entrepot;
 		this.livraisons = livraisons;
-		
+		setNoeudAVisiter();
+		setCoutPlan()
 	}
 	
-	public void calculTournee() {
+	/*
+	 * public void calculTournee() {
+	 * 
+	 * }
+	 * 
+	 * private void Dijkstra(Plan plan, Noeud entrepot) {
+	 * 
+	 * 
+	 * }
+	 */
 	
+	private Plan lirePlanXML() throws Exception {
+		DataContainer dataContainer = new DataContainer();
+		dataContainer.chargerPlan("./src/modele/grandPlan.xml");
+		return dataContainer.GetPlan();
 	}
-	private void dijkstra(Plan plan, Noeud entrepot) {
+	
+	
+	private void setNoeudAVisiter() {
+		HashSet<Noeud> ensembleNoeudAVisiter=new HashSet<Noeud>();
+		mapNoeudsAVisiter=new HashMap();
+		Set<Map.Entry< Integer,Livraison>> ensembleLivraison = livraisons.entrySet(); 
+		
+		for (Map.Entry< Integer,Livraison> it: ensembleLivraison) {
+			ensembleNoeudAVisiter.add(it.getValue().getNoeudEnlevement());
+			ensembleNoeudAVisiter.add(it.getValue().getNoeudLivraison());
+		}
+		
+		nombreNoeudAVisiter=ensembleNoeudAVisiter.size();
+		Integer indice=0;
+		
+		for(Noeud it: ensembleNoeudAVisiter) {
+			mapNoeudsAVisiter.put(indice, it);
+		}
+	}
+	
+	private void setCoutPlan() throws Exception{
+		mapNoeuds=new HashMap<Noeud,Integer>();
+		Integer indice=0;
+		Plan plan = lirePlanXML();
+		Set<Map.Entry<String, Noeud>> ensembleNoeud = plan.getNoeuds().entrySet();
+		Integer nombreNoeuds=ensembleNoeud.size();
+		List<Troncon> listeTroncons = plan.getTroncons();
+		coutPlan=new int[nombreNoeuds][nombreNoeuds];
+		
+		for(Integer i=0;i<nombreNoeuds;i++) {
+			for(Integer j=0;j<nombreNoeuds;j++) {
+				coutPlan[i][j]=Integer.MAX_VALUE;
+			}
+			coutPlan[i][i]=0;
+		}
+		
+		for (Troncon it: listeTroncons) {
+			
+			if (!mapNoeuds.containsKey(it.GetNoeudOrigine())) {
+				mapNoeuds.put(it.GetNoeudOrigine(),indice++);
+			}
+			if (!mapNoeuds.containsKey(it.GetNoeudDestination())) {
+				mapNoeuds.put(it.GetNoeudDestination(),indice++);
+			}
+			coutPlan[mapNoeuds.get(it.GetNoeudOrigine())][mapNoeuds.get(it.GetNoeudDestination())]=(int)it.GetLongueur();
+		}
 		
 	
 	}
-	private void dimensionnement() {
-		Set<Noeud> ensembleNoeud;
-		
-	}
-
+	
+	
 	/*
 	 * public FeuilleDeRoute GenererFeuilleDeRoute() {
 	 * 
