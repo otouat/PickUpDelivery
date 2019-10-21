f9dc339d084c32bd6a4d744b2aed1a133042495f
Emmylrd <emmylerandy@gmail.com>
2019-10-21 09:19:13 -0400

    Modification nommage + Suppresion du plan pour eviter superposition

--- PickUpDelivery/src/vue/TronconIHM.java
+++ PickUpDelivery/src/vue/TronconIHM.java
@@ -20,33 +20,32 @@ public class TronconIHM {
 	public static void drawTroncons(Plan plan, BorderPane paneMap) {
 		initalisationDonnees(plan,paneMap);
 		
-		
+		//Verifier si le fichier est bon ?
 		
 		List<Troncon> tronconList = plan.getTroncons();
-		
+		paneMap.getChildren().clear();
 		for(Troncon troncon : tronconList) {
 			draw(troncon,paneMap);
 		}
 	}
 	
 	public static void draw(Troncon troncon, BorderPane paneMap) {
-		int y1 = (int) -(getNewX(troncon.GetNoeudOrigine().GetLatitude()) - largeur_pane);
-		int x1 = getNewY(troncon.GetNoeudOrigine().GetLongitude());
+		int x1 = getNewX(troncon.GetNoeudOrigine().GetLongitude());
+		int y1 = (int) -(getNewY(troncon.GetNoeudOrigine().GetLatitude()) - largeur_pane);
 		
-		int y2 = (int) -(getNewX(troncon.GetNoeudDestination().GetLatitude()) -longueur_pane);
-		int x2 = getNewY(troncon.GetNoeudDestination().GetLongitude());
+		int x2 = getNewX(troncon.GetNoeudDestination().GetLongitude());
+		int y2 = (int) -(getNewY(troncon.GetNoeudDestination().GetLatitude()) -longueur_pane);
 	
-		
 		Line line = new Line(x1, y1, x2, y2);
 		paneMap.getChildren().add(line); 
 	}
 	
-	public static int getNewX(double latitude) {
+	public static int getNewY(double latitude) {
 		
 		return (int) (((latitude-latMin) * largeur_pane)/largeurPlanReel);
 	}
 	
-public static int getNewY(double longitude) {
+public static int getNewX(double longitude) {
 	return (int) (((longitude-longMin) * longueur_pane)/longueurPlanReel);
 	}
 	
