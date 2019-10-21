cb8c784a34936defc170e899bb85aea5865bcb93
tianming05 <rentianming123@outlook.com>
2019-10-21 09:34:10 +0200

    Ajout classe de test

--- PickUpDelivery/src/modele/Plan.java
+++ PickUpDelivery/src/modele/Plan.java
@@ -26,6 +26,22 @@ public class Plan {
 		return noeuds.get(idNoeud);
 	}
 
+	public Map<String, Noeud> getNoeuds() {
+		return noeuds;
+	}
+
+	public void setNoeuds(Map<String, Noeud> noeuds) {
+		this.noeuds = noeuds;
+	}
+
+	public List<Troncon> getTroncons() {
+		return troncons;
+	}
+
+	public void setTroncons(List<Troncon> troncons) {
+		this.troncons = troncons;
+	}
+
 	public void getInfo() {
 		System.out.println("nombre de noeuds: " + noeuds.size() + " nombre de troncons" + troncons.size());
 		// System.out.println(troncons.get(3).GetNomRue());
