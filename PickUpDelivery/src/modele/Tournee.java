c51472c2b82ee79b84562d121205bc7822b5b9b8
TOUAT <ousmane.touat@insa-lyon.fr>
2019-10-22 15:49:31 +0200

    Update Tournee.java

--- PickUpDelivery/src/modele/Tournee.java
+++ PickUpDelivery/src/modele/Tournee.java
@@ -1,5 +1,6 @@
 package modele;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -10,17 +11,11 @@ import java.util.Set;
 public class Tournee {
 	private Entrepot entrepot;
 	private Map<Integer, Livraison> livraisons;
-	private int nombreNoeudAVisiter;
-	private HashMap<Noeud,Integer> mapNoeudAVisiter;
-	private HashMap<Noeud,Integer> mapNoeuds;
-	private int[][] coutPlan;
-	private int[][] coutNoeudAVisiter;
+	private HashMap<Noeud,Integer> noeudAVisiter;
 
-	public Tournee(Entrepot entrepot, Map<Integer, Livraison> livraisons) throws Exception {
+	public Tournee(Entrepot entrepot, Map<Integer, Livraison> livraisons,Plan plan) {
 		this.entrepot = entrepot;
 		this.livraisons = livraisons;
-		setNoeudAVisiter();
-		setCoutPlan();
 	}
 	
 	/*
@@ -34,60 +29,43 @@ public class Tournee {
 	 * }
 	 */
 	
-	private Plan lirePlanXML() throws Exception {
-		DataContainer dataContainer = new DataContainer();
-		dataContainer.chargerPlan("./src/modele/grandPlan.xml");
-		return dataContainer.GetPlan();
-	}
 	
 	
-	private void setNoeudAVisiter() {
-		HashSet<Noeud> ensembleNoeudAVisiter=new HashSet<Noeud>();
-		mapNoeudAVisiter=new HashMap<Noeud,Integer>();
-		Set<Map.Entry< Integer,Livraison>> ensembleLivraison = livraisons.entrySet(); 
-		
-		for (Map.Entry< Integer,Livraison> it: ensembleLivraison) {
-			ensembleNoeudAVisiter.add(it.getValue().getNoeudEnlevement());
-			ensembleNoeudAVisiter.add(it.getValue().getNoeudLivraison());
-		}
-		
-		nombreNoeudAVisiter=ensembleNoeudAVisiter.size();
-		Integer indice=0;
-		
-		for(Noeud it: ensembleNoeudAVisiter) {
-			mapNoeudAVisiter.put(it,indice);
-		}
-	}
-	
-	private void setCoutPlan() throws Exception{
-		mapNoeuds=new HashMap<Noeud,Integer>();
-		Integer indice=0;
-		Plan plan = lirePlanXML();
-		Set<Map.Entry<String, Noeud>> ensembleNoeud = plan.getNoeuds().entrySet();
-		Integer nombreNoeuds=ensembleNoeud.size();
-		List<Troncon> listeTroncons = plan.getTroncons();
-		coutPlan=new int[nombreNoeuds][nombreNoeuds];
-		
-		for(Integer i=0;i<nombreNoeuds;i++) {
-			for(Integer j=0;j<nombreNoeuds;j++) {
-				coutPlan[i][j]=Integer.MAX_VALUE;
-			}
-			coutPlan[i][i]=0;
-		}
-		
-		for (Troncon it: listeTroncons) {
-			
-			if (!mapNoeuds.containsKey(it.GetNoeudOrigine())) {
-				mapNoeuds.put(it.GetNoeudOrigine(),indice++);
-			}
-			if (!mapNoeuds.containsKey(it.GetNoeudDestination())) {
-				mapNoeuds.put(it.GetNoeudDestination(),indice++);
-			}
-			coutPlan[mapNoeuds.get(it.GetNoeudOrigine())][mapNoeuds.get(it.GetNoeudDestination())]=(int)it.GetLongueur();
-		}
-		
-	
-	}
+	/*
+	 * private void setNoeudAVisiter() {
+	 * 
+	 * ArrayList<Noeud> ensembleNoeudAVisiter=new ArrayList<Noeud>();
+	 * mapNoeudAVisiter=new HashMap<Noeud,Integer>(); Set<Map.Entry<
+	 * Integer,Livraison>> ensembleLivraison = livraisons.entrySet();
+	 * 
+	 * for (Map.Entry< Integer,Livraison> it: ensembleLivraison) {
+	 * ensembleNoeudAVisiter.add(it.getValue().getNoeudEnlevement());
+	 * ensembleNoeudAVisiter.add(it.getValue().getNoeudLivraison()); }
+	 * 
+	 * nombreNoeudAVisiter=ensembleNoeudAVisiter.size(); Integer indice=0;
+	 * 
+	 * for(Noeud it: ensembleNoeudAVisiter) { mapNoeudAVisiter.put(it,indice); } }
+	 * 
+	 * static public int[][] setCoutPlan(Plan plan){ int[][] coutPlan; mapNoeuds=new
+	 * HashMap<Noeud,Integer>(); Integer indice=0; Set<Map.Entry<String, Noeud>>
+	 * ensembleNoeud = plan.getNoeuds().entrySet(); Integer
+	 * nombreNoeuds=ensembleNoeud.size(); List<Troncon> listeTroncons =
+	 * plan.getTroncons(); coutPlan=new int[nombreNoeuds][nombreNoeuds];
+	 * 
+	 * for(Integer i=0;i<nombreNoeuds;i++) { for(Integer j=0;j<nombreNoeuds;j++) {
+	 * coutPlan[i][j]=Integer.MAX_VALUE; } coutPlan[i][i]=0; }
+	 * 
+	 * for (Troncon it: listeTroncons) {
+	 * 
+	 * if (!mapNoeuds.containsKey(it.GetNoeudOrigine())) {
+	 * mapNoeuds.put(it.GetNoeudOrigine(),indice++); } if
+	 * (!mapNoeuds.containsKey(it.GetNoeudDestination())) {
+	 * mapNoeuds.put(it.GetNoeudDestination(),indice++); }
+	 * coutPlan[mapNoeuds.get(it.GetNoeudOrigine())][mapNoeuds.get(it.
+	 * GetNoeudDestination())]=(int)it.GetLongueur(); }
+	 * 
+	 * return coutPlan; }
+	 */
 	
 	
 	/*
