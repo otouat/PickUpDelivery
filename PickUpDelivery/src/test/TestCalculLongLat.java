9f2a4c80b1caf54119f0b67b6ac26f5595687453
TOUAT <ousmane.touat@insa-lyon.fr>
2019-10-23 10:32:07 +0200

    Ajout Test

---
+++ PickUpDelivery/src/test/TestCalculLongLat.java
@@ -0,0 +1,40 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import modele.DataContainer;
+import modele.Plan;
+
+public class TestCalculLongLat {
+	
+	private DataContainer dataContainer = new DataContainer();
+	
+	@Test
+	public void lireXML() {
+		try {
+			dataContainer.chargerPlan("./src/modele/grandPlan.xml");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Plan plan = dataContainer.GetPlan();
+		
+		double latmin = plan.CalculMinLatitude();
+		double latmax = plan.CalculMaxLatitude();
+		double longmin = plan.CalculMinLatitude();
+		
+		double longmax = plan.CalculMaxLatitude();
+		double ecartlat = plan.CalculEcartLatitude();
+		double ecartlong= plan.CalculEcartLongitude();
+		
+		assertEquals(45.727352,latmin,0.001);
+		assertEquals(45.780518, latmax,0.001);
+		assertEquals(0.05316599999999738, ecartlat,0.001);
+		
+		assertEquals(4.8314376, longmin,0.001);
+		assertEquals(4.9075384, longmax,0.001);
+		assertEquals(0.07610079999999986, ecartlong,0.001);
+	
+	}
+
+
+}
