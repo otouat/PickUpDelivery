b93d64b98f3d6b46713baadf77b31a355ee31968
TOUAT <ousmane.touat@insa-lyon.fr>
2019-11-04 09:54:54 +0100

    Ajout fonction bound TSP2

--- PickUpDelivery/src/modele/TSP2.java
+++ PickUpDelivery/src/modele/TSP2.java
@@ -7,6 +7,28 @@ public class TSP2 extends TSP1{
 	
 	@Override
 	protected int bound(Integer sommetCourant, ArrayList<Integer> nonVus, int[][] cout, int[] duree) {
-		return 0;
+		Integer somme=0;
+		Integer minCoutNdVoisin=Integer.MAX_VALUE;
+		
+		
+		for(Integer i : nonVus) {
+			if(cout[sommetCourant][i]<minCoutNdVoisin) {
+				minCoutNdVoisin=cout[sommetCourant][i];
+			}
+		}
+		somme+=minCoutNdVoisin; // ajout du cout du chemin sommetCourant vers le noeud le plus proche
+		for(Integer i : nonVus) {
+			Integer min=cout[i][0];
+			for(Integer j : nonVus) {
+				if( (cout[i][j]<min) && (i!=j)) {
+					min=cout[i][j];
+				}
+			somme+=min+duree[i];
+			}
+		}
+		
+		return somme; 
+		//retourne la borne inferieur du cout des permutation passant
+		//par tous les noeuds non vus et terminant par l'entrepot
 	}
 }
