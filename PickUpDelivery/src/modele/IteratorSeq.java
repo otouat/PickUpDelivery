9f8c0f37d4a22cd6c176e55313602319fbf1ea5b
agadachach <amine.gadacha-charrad@insa-lyon.fr>
2019-11-06 09:37:26 +0100

    IteratorSeq

--- PickUpDelivery/src/modele/IteratorSeq.java
+++ PickUpDelivery/src/modele/IteratorSeq.java
@@ -7,6 +7,7 @@ public class IteratorSeq implements Iterator<Integer> {
 
 	private Integer[] candidats;
 	private int nbCandidats;
+	private Integer[][] precedence;
 
 	/**
 	 * Cree un iterateur pour iterer sur l'ensemble des sommets de nonVus
@@ -17,7 +18,9 @@ public class IteratorSeq implements Iterator<Integer> {
 		this.candidats = new Integer[nonVus.size()];
 		nbCandidats = 0;
 		for (Integer s : nonVus){
-			candidats[nbCandidats++] = s;
+			if(!nonVus.contains(precedence[s][1])) {
+				candidats[nbCandidats++] = s;
+			}
 		}
 	}
 	
