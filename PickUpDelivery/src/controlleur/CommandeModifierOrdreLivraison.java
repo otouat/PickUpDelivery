2507c5403cf7601e5616e63f14f37c748fe8d666
jbrouboni <45970166+jbrouboni@users.noreply.github.com>
2019-11-13 22:40:15 +0100

    Toutes les Commandes ont été crées.Go Implémenter la dernière

---
+++ PickUpDelivery/src/controlleur/CommandeModifierOrdreLivraison.java
@@ -0,0 +1,49 @@
+package controlleur;
+
+import java.util.List;
+
+import modele.Livraison;//
+import modele.Noeud;
+import modele.Tournee;
+import vue.MainControlleur;
+import vue.VueTroncon;
+//TOUT EST A MODIFIER
+public class CommandeModifierOrdreLivraison implements Commande {
+
+	private Livraison livraison;
+	private Tournee tournee;
+	private Noeud noeudChange;
+	private Noeud noeudActuel;
+	private int rangNoeudAChanger;
+	private MainControlleur fenetre;
+	
+
+	
+	public CommandeModifierOrdreLivraison(Livraison livraison, Tournee tournee, Noeud noeudChange, Noeud noeudActuel,
+			int rangNoeudAChanger, MainControlleur fenetre) {
+		super();
+		this.livraison = livraison;
+		this.tournee = tournee;
+		this.noeudChange = noeudChange;
+		this.noeudActuel = noeudActuel;
+		this.rangNoeudAChanger = rangNoeudAChanger;
+		this.fenetre = fenetre;
+	}
+
+	@Override
+	public void doCommande() {
+		// TODO Auto-generated method stub
+		List<Noeud> listeNoeuds = tournee.recalculTourneeApresChangementNoeud(noeudChange,rangNoeudAChanger);
+		fenetre.reInitialiseListView(tournee.getenchainementNoeudAVisiterAvecInfos());
+		VueTroncon.drawTournee(listeNoeuds, fenetre.tourneePane);
+	}
+
+	@Override
+	public void undoCommande() {
+		// TODO Auto-generated method stub
+		List<Noeud> listeNoeuds = tournee.recalculTourneeApresChangementNoeud(noeudActuel,rangNoeudAChanger);
+		fenetre.reInitialiseListView(tournee.getenchainementNoeudAVisiterAvecInfos());
+		VueTroncon.drawTournee(listeNoeuds, fenetre.tourneePane);
+	}
+
+}
