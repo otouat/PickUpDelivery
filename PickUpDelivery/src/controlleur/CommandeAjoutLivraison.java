01f03ae78042db2121410cd2920b189133207037
sofiane-dev <sofiane.benslimane.pro@gmail.com>
2019-11-13 17:08:53 +0100

    pattern commande

--- PickUpDelivery/src/controlleur/CommandeAjoutLivraison.java
+++ PickUpDelivery/src/controlleur/CommandeAjoutLivraison.java
@@ -8,24 +8,12 @@ public class CommandeAjoutLivraison implements Commande {
 
 	private Livraison livraison;
 	private Tournee tournee;
-	//private int positionAjout;
-	//private int rangPreEnlevement;
-	//private int rangPreLivraison;
 	private Noeud noeudBeforePickUp;
 	private Noeud noeudBeforeDelivery;
 
 	/**
 	 * Cree la commande qui ajoute a la position position la livraison livraison
-	 * 
-	 * @param position
-	 * @param livraison
-	 * @param calculateurTournee
 	 */
-	/*CommandeAjoutLivraison(int positionAjout, Livraison livraison, Tournee tournee) {
-		this.livraison = livraison;
-		this.tournee = tournee;
-		this.positionAjout = positionAjout;
-	}*/
 	public CommandeAjoutLivraison(Noeud noeudBeforePickUp, Noeud noeudBeforeDelivery, Livraison livraison, Tournee tournee) {
 		this.livraison = livraison;
 		this.tournee = tournee;
--- PickUpDelivery/src/controlleur/CommandeSuppressionLivraison.java
+++ PickUpDelivery/src/controlleur/CommandeSuppressionLivraison.java
@@ -7,10 +7,6 @@ public class CommandeSuppressionLivraison implements Commande {
 
 	private Livraison livraison;
 	private Tournee tournee;
-	private int positionAjout;
-	private int rangPreEnlevement;
-	private int rangPreLivraison;
-
 	/**
 	 * Cree la commande qui ajoute a la position position la livraison livraison
 	 * 
@@ -18,10 +14,9 @@ public class CommandeSuppressionLivraison implements Commande {
 	 * @param livraison
 	 * @param calculateurTournee
 	 */
-	CommandeSuppressionLivraison(int positionAjout, Livraison livraison, Tournee tournee) {
+	CommandeSuppressionLivraison(Livraison livraison, Tournee tournee) {
 		this.livraison = livraison;
 		this.tournee = tournee;
-		this.positionAjout = positionAjout; //
 	}
 
 	@Override
@@ -35,7 +30,7 @@ public class CommandeSuppressionLivraison implements Commande {
 	@Override
 	public void undoCommande() {
 		
-		tournee.recalculTourneeApresAjoutLivraison( livraison,rangPreEnlevement,rangPreLivraison);
+		//tournee.recalculTourneeApresAjoutLivraison( livraison,rangPreEnlevement,rangPreLivraison);
 		//TODO : modif graphique
 	}
 
